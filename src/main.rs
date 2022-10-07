3e623d48f5a18c0678436198fc7ce97f42e000c5
Anton Eriksson <antoneriksson978@gmail.com>
2022-10-07 10:07:53 +0200

    Add simple cli make a move demo

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use inquire;
+use shakmaty::{Chess, Position};
+
 fn main() {
-    println!("Hello, world!");
+    let starting_pos = Chess::default();
+    let moves = starting_pos.legal_moves();
+    let white_moves_alg: Vec<String> = moves.iter().map(|m| m.to_string()).collect();
+    print!("{}", white_moves_alg.len().to_string());
+    let ans = inquire::Select::new("Make a move:", white_moves_alg).prompt();
+    match ans {
+        Ok(m) => println!("Played {}", m),
+        Err(_) => println!("Invalid move."),
+    }
 }
